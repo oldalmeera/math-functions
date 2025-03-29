0c6fbffc9659f3b8a5fd87617f7d0e0ec66488a6
oldalmeera <oldalmeera@gmail.com>
2025-03-29 19:32:25 +0000

    Add 19.rs

---
+++ src/19.rs
@@ -0,0 +1,21 @@
+// Example Rust code with mathematical operations
+fn main() {
+    let num1 = 5;
+    let num2 = 3;
+
+    // Addition
+    let sum: i32 = num1 + num2;
+    println!("The sum of {} and {}", num1, num2, "is", sum);
+
+    // Subtraction
+    let difference: i32 = num1 - num2;
+    println!("The difference between {} and {}", num1, num2, "is", difference);
+
+    // Multiplication
+    let product: i32 = num1 * num2;
+    println!("The product of {} and {}", num1, num2, "is", product);
+
+    // Division
+    let quotient: i32 = num1 / num2;
+    println!("The quotient when dividing {} by {} is", num1, num2, "is", quotient);
+}
