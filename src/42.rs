26d3939e9a40de552599b3b6c199f3ab6d9df680
oldalmeera <oldalmeera@gmail.com>
2025-05-27 13:04:49 +0000

    Add 42.rs

---
+++ src/42.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Example usage of math functions
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    let y = 3.14;
+    println!("The value of y is: {}", y);
+
+    let z = 2.71828182846;
+    println!("The value of z is: {}", z);
+}
